d701a1154d28c8f37aea56e573b67a6546518eac
Oleksandr Minko <oleksandr.minko@cengage.com>
2024-04-28 20:44:27 +0200

    init commit

---
+++ src/main/java/com/futuretech/Demoappec2Application.java
@@ -0,0 +1,12 @@
+package com.futuretech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoappec2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Demoappec2Application.class, args);
+  }
+}
---
+++ src/test/java/com/futuretech/Demoappec2ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.futuretech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoappec2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
